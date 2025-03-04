6af8cdeef11b38af9bc06c3a09424d2f9e95b786
YarosTs8191 <143120910+YarosTs8191@users.noreply.github.com>
2025-03-04 14:44:33 +0100

    oop

--- js/script.js
+++ js/script.js
@@ -20,3 +20,11 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+const form = document.querySelector(".feedback-form");
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  console.log(evt.target.elements.message.value);
+  form.reset();
+});
